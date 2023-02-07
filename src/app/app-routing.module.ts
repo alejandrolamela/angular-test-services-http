7b6e81f2838f4437a49e3d403b18b0a7dcec33df
Alejandro Lam <alejandrolam@Alejandros-MacBook-Air.local>
2023-02-07 11:11:08 +0100

    first: config routes and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
+import { CharacterComponent } from './pages/character/character.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'characters', component: CharacterComponent
+  },
+  {
+    path:'character/:id', component: CharacterDetailComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
+import { CharacterComponent } from './pages/character/character.component';
+import { CharacterDetailComponent } from './pages/character-detail/character-detail.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigatorComponent } from './shared/components/navigator/navigator.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterComponent,
+    CharacterDetailComponent,
+    HomeComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
